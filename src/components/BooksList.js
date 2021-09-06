b82cb3ac14386de4715ecbb85b02f5b070880f2d
Binyam <binyam@pop-os.localdomain>
2021-09-06 19:12:40 +0300

    Update BooksList.js
    - connect BooksList to redux store using connect method
    - update BooksList component to render booksRow from books state
    from redux store

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,16 +1,30 @@
-const BooksList = () => (
-        <table>
-            <tr> 
-                <th>Book ID</th>
-                <th>title</th>
-                <th>category</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>my book</td>
-                <td>fictional</td>
-            </tr>
-        </table>
-    )
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Book from './Book'
+import { mapStateToProps, mapDispatchToProps } from "../reducers";
 
-export default BooksList;
+const BooksList = ({books}) => {
+        const booksRow = books.map((book)=>
+            <Book key={book.id} id={book.id} title={book.title} category={book.category}/>
+        )
+        return (
+            <table>
+            <thead>
+                <tr> 
+                    <th>Book ID</th>
+                    <th>title</th>
+                    <th>category</th>
+                </tr>
+            </thead>
+            <tbody>
+                {booksRow}
+            </tbody>
+        </table>
+        )
+    }
+
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
+
+BooksList.propTypes = {
+    books: PropTypes.instanceOf(Array).isRequired
+}   
