7c9c15132f871741822b0abce0d1248afbece963
Binyam <binyam@pop-os.localdomain>
2021-09-06 19:11:06 +0300

    Update BooksForm
    - add bookCategories as an array and render inside select tag
    as categoryOptions

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,12 +1,17 @@
-const BooksForm = () => (
+const BooksForm = () => {
+    const bookCategories =["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
+    const categoryOptions = bookCategories.map((category)=>
+        <option key={category}>{category}</option>
+    )
+    return (
         <form>
             <input type="text" id="title"/>
             <select>
-                <option>first option</option>
-                <option>second option</option>
+                {categoryOptions}
             </select>
             <input type="submit" value="Add Book"/>
         </form>
     )
+}
 
 export default BooksForm;   
