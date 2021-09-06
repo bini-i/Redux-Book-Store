504e3ae7143a286a6806a60c29650238ee01cdc7
Binyam <binyam@pop-os.localdomain>
2021-09-06 19:14:20 +0300

    Update src/reducers/books.js
    - udpate to reducer to switch over CREATE_BOOK and REMOVE_BOOK cases
    and update the state accordingly

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,9 +1,18 @@
-const booksReducer = (state = [], actions)=> {
-    switch(actions.type){
+import { CREATE_BOOK, REMOVE_BOOK } from "../actions";
+
+const booksReducer = (state = [], action)=> {
+    switch(action.type){
+        case CREATE_BOOK:
+            return [...state, action.book]
+        case REMOVE_BOOK:
+            if(state.includes(action.book)){
+                state.filter((ele)=>ele!==action.book)
+            }
+            return []
         default:
             return state;
     }
 }
 
-export default booksReducer
+export default booksReducer;
 
