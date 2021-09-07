32788d8be92e5d3534513168c760c2086f29aff2
Binyam <binyam@pop-os.localdomain>
2021-09-07 12:10:26 +0300

    Update Book.js
    - connect the component to redux store using only dispatch as props
    - add a remove book button to each row of books
    - add handleRemoveBook method that handles removing a book
      from the redux store

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,32 @@
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { mapDispatchToProps } from "../reducers";
 
-const Book = ({id, title, category}) => (
-        <tr>
-            <td>{id}</td>
-            <td>{title}</td>
-            <td>{category}</td>
-        </tr>
-    )
+const Book = ({id, title, category, removeBook}) => {
+        const handleRemoveBook = () => {
+            removeBook({id, title, category})
+        }   
+        return (
+            <tr>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>{category}</td>
+                <td>
+                    <button onClick={handleRemoveBook} name='removeBook' type="button" value='Remove book'>
+                        Remove book
+                    </button> 
+                </td>
+            </tr>
+        )
+    }
 
-export default Book;
+export default connect(null, mapDispatchToProps)(Book);
 
 
 Book.propTypes = {
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
+    removeBook: PropTypes.func.isRequired
 };
   
