97d0bb862481363fb473e3f36178e6296a8d6fda
Binyam <binyam@pop-os.localdomain>
2021-09-07 16:01:07 +0300

    Update Book.js
    - remove handleRemoveBook event handler and placed it
      inside BooksList component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,32 +1,25 @@
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { mapDispatchToProps } from "../reducers";
 
-const Book = ({id, title, category, removeBook}) => {
-        const handleRemoveBook = () => {
-            removeBook({id, title, category})
-        }   
-        return (
+const Book = ({id, title, category, handleRemoveBook}) => (
             <tr>
                 <td>{id}</td>
                 <td>{title}</td>
                 <td>{category}</td>
                 <td>
-                    <button onClick={handleRemoveBook} name='removeBook' type="button" value='Remove book'>
+                    <button onClick={handleRemoveBook.bind(this, {id, title, category})} name='removeBook' type="button" value='Remove book'>
                         Remove book
                     </button> 
                 </td>
             </tr>
         )
-    }
 
-export default connect(null, mapDispatchToProps)(Book);
+export default Book;
 
 
 Book.propTypes = {
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
-    removeBook: PropTypes.func.isRequired
+    handleRemoveBook: PropTypes.func.isRequired
 };
   
