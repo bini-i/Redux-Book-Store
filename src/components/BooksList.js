4732c8e044af5563f18488af0449d2b8a68b71a8
Binyam <binyam@pop-os.localdomain>
2021-09-08 08:03:53 +0300

    Update BooksList component
    - renders CategoryFilter and passes filter state and
      handleFilterChange as props
    - add handleFilterChange method that handels change
      of filter state

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,29 +2,47 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Book from './Book'
 import { mapStateToProps, mapDispatchToProps } from "../reducers";
+import CategoryFilter from "./CategoryFilter";
 
-const BooksList = ({books, removeBook}) => {
+const BooksList = ({books, removeBook, filter, changeFilter}) => {
         const handleRemoveBook = ({id,title,category}) => {
             removeBook({id, title, category})
         } 
 
-        const booksRow = books.map((book)=>
+        const handleFilterChange = (event) => {
+            changeFilter(event.target.value)
+        }
+
+        const filteredBooks = () => {
+            if(filter!=='ALL'){
+                return books.filter((book) => book.category === filter)
+            }
+            return books
+        }
+
+        const booksRow = filteredBooks().map((book)=>
             <Book key={book.id} id={book.id} title={book.title} category={book.category} handleRemoveBook={handleRemoveBook}/>
         )
 
         return (
-            <table>
-            <thead>
-                <tr> 
-                    <th>Book ID</th>
-                    <th>title</th>
-                    <th>category</th>
-                </tr>
-            </thead>
-            <tbody>
-                {booksRow}
-            </tbody>
-        </table>
+            <>
+                <table>
+                    <thead>
+                        <tr> 
+                            <th>Book ID</th>
+                            <th>title</th>
+                            <th>category</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {booksRow}
+                    </tbody>
+                </table>
+                <div className='filter'>
+                    <span>Filter Books</span>
+                    <CategoryFilter filter={filter} handleFilterChange={handleFilterChange}/>
+                </div>
+            </>
         )
     }
 
@@ -32,5 +50,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
 
 BooksList.propTypes = {
     books: PropTypes.instanceOf(Array).isRequired,
-    removeBook: PropTypes.func.isRequired
+    removeBook: PropTypes.func.isRequired,
+    filter: PropTypes.string.isRequired,
+    changeFilter: PropTypes.func.isRequired,
 }   
