137d9443038225b5e95a4e1e2f5e7567ad54cfa6
Binyam <binyam@pop-os.localdomain>
2021-09-09 10:31:12 +0300

    Update Book component
    - substitute table of book rows with styled HTML

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,39 @@
 import PropTypes from "prop-types";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Box from '@material-ui/core/Box';
 
 const Book = ({id, title, category, handleRemoveBook}) => (
-            <tr>
-                <td>{id}</td>
-                <td>{title}</td>
-                <td>{category}</td>
-                <td>
-                    <button onClick={handleRemoveBook.bind(this, {id, title, category})} name='removeBook' type="button" value='Remove book'>
-                        Remove book
-                    </button> 
-                </td>
-            </tr>
+            <>
+                <section className='book'>
+                    <div className='book-info'>
+                        <span className='book-category'>{category}</span>
+                        <h2 className='book-title'>{title}</h2>
+                        <span className='book-author'>Suzanne Collins</span>
+
+                        <div className='opts'>
+                            <button type='button'>Comments</button>
+                            <button type='button' onClick={handleRemoveBook.bind(this, {id, title, category})}>Remove</button>
+                            <button type='button'>Edit</button>
+                        </div>
+                    </div>
+
+                    <div className='progress'>
+                        <Box className='progress-indicator' position="relative" display="inline-flex">
+                            <CircularProgress variant="determinate" value = '64'/>
+                            <Box className='container'>
+                                <p className='progress-percent'>64%</p>
+                                <p className='progress-status'>Completed</p>
+                            </Box>
+                        </Box>
+
+                        <div className='progress-detail'>
+                            <p className='chapter-txt'>CURRENT CHAPTER</p>
+                            <p className='chapter'>Chapter 17</p>
+                            <button className='update-btn' type='button'>UPDATE PROGRESS</button>
+                        </div>
+                    </div>
+                </section>
+            </>
         )
 
 export default Book;
